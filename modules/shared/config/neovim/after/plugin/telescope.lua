b24fdbed109bcd9b142b64b2ff69cec335a7c18c
Cal <accounts@cal.rip>
2024-02-21 15:32:43 +0000

    feat: telescope grep args

--- modules/shared/config/neovim/after/plugin/telescope.lua
+++ modules/shared/config/neovim/after/plugin/telescope.lua
@@ -19,6 +19,13 @@ require("telescope").setup({
 			"truncate",
 		},
 	},
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown({
+				-- even more opts
+			}),
+		},
+	},
 })
 
 -- truncate file paths in find files
@@ -29,6 +36,8 @@ pcall(require("telescope").load_extension, "fzf")
 
 -- Enable telescope harpoon fun, if installed
 pcall(require("telescope").load_extension, "harpoon")
+pcall(require("telescope").load_extension, "ui-select")
+pcall(require("telescope").load_extension, "live_grep_args")
 
 -- See `:help telescope.builtin`
 vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
@@ -46,5 +55,11 @@ vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { des
 vim.keymap.set("n", "<leader>sb", require("telescope.builtin").buffers, { desc = "[S]earch [B]uffers" })
 vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
 vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
-vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+-- vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set(
+	"n",
+	"<leader>sg",
+	":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+	{ desc = "[S]earch by [G]rep", silent = true }
+)
 vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
--- modules/shared/config/neovim/lua/caligula/plugins.lua
+++ modules/shared/config/neovim/lua/caligula/plugins.lua
@@ -322,7 +322,20 @@ require("lazy").setup({
 	{ "ThePrimeagen/harpoon", dependencies = { "nvim-lua/plenary.nvim" } },
 
 	-- Fuzzy Finder (files, lsp, etc)
-	{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+	"nvim-telescope/telescope-ui-select.nvim",
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				-- This will not install any breaking changes.
+				-- For major updates, this must be adjusted manually.
+				version = "^1.0.0",
+			},
+		},
+	},
 
 	-- Fuzzy Finder Algorithm which requires local dependencies to be built.
 	-- Only load if `make` is available. Make sure you have the system
