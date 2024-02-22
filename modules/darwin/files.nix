bc098bbb525a8a216a30e870e106dfed7a174ca7
Cal <accounts@cal.rip>
2024-02-22 08:51:54 +0000

    fix: tidy up home.file

--- modules/darwin/files.nix
+++ modules/darwin/files.nix
@@ -1,19 +1,8 @@
 { user, config, ... }:
 
 let
-  xdg_configHome = "${config.users.users.${user}.home}/.config";
+  xdgConfigHome = "${config.users.users.${user}.home}/.config";
 in
 {
-
-  #"${xdg_configHome}/skhd/skhdrc" = {
-  #  text = ''
-  #    hyper - a : /usr/bin/open -a Alacritty
-  #    hyper - s : /usr/bin/open -a Arc
-  #    hyper - d : /usr/bin/open -a Slack
-  #  '';
-  #};
-
-  "${xdg_configHome}/karabiner/karabiner.json" = {
-    text = builtins.readFile ../darwin/config/karabiner/karabiner.json;
-  };
+  "${xdgConfigHome}/karabiner".source =  ./config/karabiner;
 }
--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -1,68 +1,14 @@
 { user, config, ... }:
 
 let
-  xdg_confighome = "${config.users.users.${user}.home}/.config";
-  neovimconfighome = "${config.users.users.${user}.home}/.config/nvim";
+  xdgConfigHome = "${config.users.users.${user}.home}/.config";
+  neovimConfigHome = "${config.users.users.${user}.home}/.config/nvim";
 in
 {
-  ".hushlogin" = {
-    text = "";
-  };
-  "${neovimconfighome}/init.lua" = {
-    text = builtins.readFile ./config/neovim/init.lua;
-  };
-  "${neovimconfighome}/lua/caligula/init.lua" = {
-    text = builtins.readFile ./config/neovim/lua/caligula/init.lua;
-  };
-  "${neovimconfighome}/lua/caligula/autocmds.lua" = {
-    text = builtins.readFile ./config/neovim/lua/caligula/autocmds.lua;
-  };
-  "${neovimconfighome}/lua/caligula/plugins.lua" = {
-    text = builtins.readFile ./config/neovim/lua/caligula/plugins.lua;
-  };
-  "${neovimconfighome}/lua/caligula/remap.lua" = {
-    text = builtins.readFile ./config/neovim/lua/caligula/remap.lua;
-  };
-  "${neovimconfighome}/lua/caligula/set.lua" = {
-    text = builtins.readFile ./config/neovim/lua/caligula/set.lua;
-  };
-  "${neovimconfighome}/after/plugin/cmp.lua" = {
-    text = builtins.readFile ./config/neovim/after/plugin/cmp.lua;
-  };
-  "${neovimconfighome}/after/plugin/colour.lua" = {
-    text = builtins.readFile ./config/neovim/after/plugin/colour.lua;
-  };
-  "${neovimconfighome}/after/plugin/harpoon.lua" = {
-    text = builtins.readFile ./config/neovim/after/plugin/harpoon.lua;
-  };
-  "${neovimconfighome}/after/plugin/lsp.lua" = {
-    text = builtins.readFile ./config/neovim/after/plugin/lsp.lua;
-  };
-  "${neovimconfighome}/after/plugin/telescope.lua" = {
-    text = builtins.readFile ./config/neovim/after/plugin/telescope.lua;
-  };
-  "${neovimconfighome}/after/plugin/treesitter.lua" = {
-    text = builtins.readFile ./config/neovim/after/plugin/treesitter.lua;
-  };
-  "${neovimconfighome}/after/plugin/folds.lua" = {
-    text = builtins.readFile ./config/neovim/after/plugin/folds.lua;
-  };
-  "${neovimconfighome}/after/plugin/noice.lua" = {
-    text = builtins.readFile ./config/neovim/after/plugin/noice.lua;
-  };
-  "${xdg_confighome}/tmuxinator/mono.yml" = {
-    text = builtins.readFile ./config/tmuxinator/mono.yml;
-  };
-  "${xdg_confighome}/tmuxinator/players.yml" = {
-    text = builtins.readFile ./config/tmuxinator/players.yml;
-  };
-  "${xdg_confighome}/tmuxinator/r18.yml" = {
-    text = builtins.readFile ./config/tmuxinator/r18.yml;
-  };
-  "${xdg_confighome}/spotify-player/app.toml" = {
-    text = builtins.readFile ./config/spotify-player/app.toml;
-  };
-  "${xdg_confighome}/spotify-player/theme.toml" = {
-    text = builtins.readFile ./config/spotify-player/theme.toml;
-  };
+  ".hushlogin".text = "";
+  "${neovimConfigHome}/init.lua".source = ./config/neovim/init.lua;
+  "${neovimConfigHome}/lua".source =  ./config/neovim/lua;
+  "${neovimConfigHome}/after".source = ./config/neovim/after;
+  "${xdgConfigHome}/tmuxinator".source =  ./config/tmuxinator;
+  "${xdgConfigHome}/spotify-player".source = ./config/spotify-player;
 }
