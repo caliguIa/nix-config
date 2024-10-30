1bbdffdbb27b0808ad9241f7e8e7f55e46aa1044
Cal <acc@calrichards.io>
2024-10-30 23:21:51 +0000

    feat: refactor keymaps using factories

--- modules/shared/config/wezterm/lua/keys.lua
+++ modules/shared/config/wezterm/lua/keys.lua
@@ -5,22 +5,28 @@ local run = require "lua/launchers/run"
 
 local act = wezterm.action
 
+local make_leader_action = utils.make_leader_action
+local make_pane_nav = utils.make_pane_nav
+local make_pane_resize = utils.make_pane_resize
+
 local M = {}
 
 M.keys = {
     workspaces.keymap,
     run.keymap,
-    {
-        mods = "LEADER",
-        key = "s",
-        action = act.ShowLauncherArgs {
+
+    -- Fuzzy search workspaces
+    make_leader_action(
+        "s",
+        act.ShowLauncherArgs {
             flags = "FUZZY|WORKSPACES",
-        },
-    },
-    {
-        key = "t",
-        mods = "LEADER",
-        action = wezterm.action.SplitPane {
+        }
+    ),
+
+    -- Custom todo pane action
+    make_leader_action(
+        "t",
+        wezterm.action.SplitPane {
             direction = "Right",
             command = {
                 args = {
@@ -30,78 +36,28 @@ M.keys = {
                     "nvim todo/$(git branch --show-current)",
                 },
             },
-        },
-    },
-    {
-        key = "d",
-        mods = "LEADER",
-        action = act.CloseCurrentPane { confirm = true },
-    },
-    {
-        mods = "LEADER",
-        key = "h",
-        action = act.SplitVertical { domain = "CurrentPaneDomain" },
-    },
-    {
-        mods = "LEADER",
-        key = "v",
-        action = act.SplitHorizontal { domain = "CurrentPaneDomain" },
-    },
-    {
-        mods = "LEADER",
-        key = "c",
-        action = act.SpawnTab "CurrentPaneDomain",
-    },
-    {
-        mods = "LEADER",
-        key = "m",
-        action = act.TogglePaneZoomState,
-    },
-    {
-        key = "Enter",
-        mods = "LEADER",
-        action = wezterm.action.ActivateCopyMode,
-    },
-    {
-        key = "h",
-        mods = "CMD|CTRL|SHIFT|OPT",
-        action = act.ActivatePaneDirection "Left",
-    },
-    {
-        key = "l",
-        mods = "CMD|CTRL|SHIFT|OPT",
-        action = act.ActivatePaneDirection "Right",
-    },
-    {
-        key = "k",
-        mods = "CMD|CTRL|SHIFT|OPT",
-        action = act.ActivatePaneDirection "Up",
-    },
-    {
-        key = "j",
-        mods = "CMD|CTRL|SHIFT|OPT",
-        action = act.ActivatePaneDirection "Down",
-    },
-    {
-        key = "H",
-        mods = "LEADER",
-        action = act.AdjustPaneSize { "Left", 15 },
-    },
-    {
-        key = "J",
-        mods = "LEADER",
-        action = act.AdjustPaneSize { "Down", 15 },
-    },
-    {
-        key = "K",
-        mods = "LEADER",
-        action = act.AdjustPaneSize { "Up", 15 },
-    },
-    {
-        key = "L",
-        mods = "LEADER",
-        action = act.AdjustPaneSize { "Right", 15 },
-    },
+        }
+    ),
+
+    -- Pane management
+    make_leader_action("d", act.CloseCurrentPane { confirm = true }),
+    make_leader_action("h", act.SplitVertical { domain = "CurrentPaneDomain" }),
+    make_leader_action("v", act.SplitHorizontal { domain = "CurrentPaneDomain" }),
+    make_leader_action("c", act.SpawnTab "CurrentPaneDomain"),
+    make_leader_action("m", act.TogglePaneZoomState),
+    make_leader_action("Enter", wezterm.action.ActivateCopyMode),
+
+    -- Pane navigation (with quad modifier)
+    make_pane_nav("Left", "h"),
+    make_pane_nav("Right", "l"),
+    make_pane_nav("Up", "k"),
+    make_pane_nav("Down", "j"),
+
+    -- Pane resizing
+    make_pane_resize "Left",
+    make_pane_resize "Down",
+    make_pane_resize "Up",
+    make_pane_resize "Right",
 }
 
 return M
--- modules/shared/config/wezterm/lua/utils.lua
+++ modules/shared/config/wezterm/lua/utils.lua
@@ -1,4 +1,5 @@
 local wezterm = require "wezterm"
+local act = wezterm.action
 
 local M = {}
 
@@ -11,6 +12,28 @@ function M.merge_tables(...)
     end
     return result
 end
+
+function M.make_leader_action(key, action)
+    return {
+        mods = "LEADER",
+        key = key,
+        action = action,
+    }
+end
+
+function M.make_hyper_key_action(key, action)
+    return {
+        key = key,
+        mods = "CMD|CTRL|SHIFT|OPT",
+        action = action,
+    }
+end
+
+function M.make_pane_resize(direction)
+    return M.make_leader_action(direction:sub(1, 1):upper(), act.AdjustPaneSize { direction, 15 })
+end
+
+function M.make_pane_nav(direction, key) return M.make_hyper_key_action(key, act.ActivatePaneDirection(direction)) end
 
 M.homeDir = wezterm.home_dir
 
