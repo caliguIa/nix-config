a355462ae598c06b45c65d51251e1c06d76df94d
Cal <acc@calrichards.io>
2024-10-31 10:52:51 +0000

    fix: remove theme from ncspot

--- modules/shared/packages/ncspot.nix
+++ modules/shared/packages/ncspot.nix
@@ -103,25 +103,5 @@
       "?" = "help";
       "Shift+u" = "update";
     };
-    # # Gruvbox dark [ originally based on base16 by Dawid Kurek (dawikur@gmail.com) ];
-    # theme = {
-    #   background = "default";
-    #   primary = "#a89984";
-    #   secondary = "#928374";
-    #   title = "#8ec07c";
-    #   playing = "#689d6a";
-    #   playing_bg = "#383838";
-    #   playing_selected = "#ebdbb2";
-    #   highlight = "#d5c4a1";
-    #   highlight_bg = "#484848";
-    #   error = "#fbf1c7";
-    #   error_bg = "#cc241d";
-    #   statusbar_progress = "#458588";
-    #   statusbar_bg = "#282828";
-    #   statusbar = "#98971a";
-    #   cmdline = "#d5c4a1";
-    #   cmdline_bg = "#383838";
-    #   search_match = "#fabd2f";
-    # };
   };
 }
