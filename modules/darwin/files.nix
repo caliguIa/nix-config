d0c005290f3d31351c28ca1821f5d56b46460f61
Cal <acc@calrichards.io>
2024-11-02 13:06:06 +0000

    feat: setup aerospace

--- modules/darwin/files.nix
+++ modules/darwin/files.nix
@@ -4,5 +4,6 @@ let
   xdgConfigHome = "${config.users.users.${user}.home}/.config";
 in
 {
-  "${xdgConfigHome}/karabiner".source =  ./config/karabiner;
+  "${xdgConfigHome}/karabiner".source = ./config/karabiner;
+  "${xdgConfigHome}/aerospace".source = ./config/aerospace;
 }
