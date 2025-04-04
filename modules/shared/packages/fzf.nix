eaada45644cc74cbd37f59800f5fd490f6fe4e8d
Cal <acc@calrichards.io>
2025-04-04 08:34:16 +0100

    tmux fzf command launcher

--- modules/shared/packages/fzf.nix
+++ modules/shared/packages/fzf.nix
@@ -4,35 +4,24 @@
   enableZshIntegration = true;
   defaultCommand = "fd --type f --hidden --follow --exclude .git --exclude .vim --exclude .cache --exclude vendor --exclude node_modules";
   defaultOptions = [ "--inline-info" ];
+  tmux = {
+    enableShellIntegration = true;
+  };
   colors = {
-    # melange
-
-    bg = "#292522";
-    "bg+" = "#292522";
-    fg = "#ECE1D7";
-    "fg+" = "#ECE1D7";
-    hl = "#ff9e64";
-    "hl+" = "#ff9e64";
-    info = "#EBC06D";
-    prompt = "#A3A9CE";
-    pointer = "#A3A9CE";
-    marker = "#A3A9CE";
-    spinner = "#A3A9CE";
-    header = "#9ece6a";
-
-    # tokyonight
-
-    # bg = "#1a1b26";
-    # "bg+" = "#292e42";
-    # fg = "#c0caf5";
-    # "fg+" = "#c0caf5";
-    # hl = "#ff9e64";
-    # "hl+" = "#ff9e64";
-    # info = "#7aa2f7";
-    # prompt = "#7dcfff";
-    # pointer = "#7dcfff";
-    # marker = "#9ece6a";
-    # spinner = "#9ece6a";
-    # header = "#9ece6a";
+    bg = "#121212";
+    "bg+" = "#121212";
+    preview-bg = "#121212";
+    fg = "#b4b4b4";
+    "fg+" = "#f5f5f5";
+    preview-fg = "#b4b4b4";
+    hl = "#ff0088";
+    "hl+" = "#f5f5f5";
+    info = "#d5d5d5";
+    border = "#121212";
+    prompt = "#ff0088";
+    pointer = "#ff0088";
+    marker = "#ff0088";
+    spinner = "#ff0088";
+    header = "#ff0088";
   };
 }
