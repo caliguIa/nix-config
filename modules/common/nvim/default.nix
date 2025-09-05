d7c0d3e9a92810d1936d3765a1737845d307091b
Cal <acc@calrichards.io>
2025-09-05 12:29:56 +0100

    cleanup nvim overlay

--- modules/common/nvim/default.nix
+++ modules/common/nvim/default.nix
@@ -1,45 +1,15 @@
-{inputs, ...} @ attrs: let
+{inputs, ...}: let
     inherit (inputs) nixpkgs;
     inherit (inputs.nixCats) utils;
     luaPath = "${./.}";
     forEachSystem = utils.eachSystem nixpkgs.lib.platforms.all;
     themeConfig = import ../themes;
-    extra_pkg_config = {
-        allowUnfree = true;
-    };
-    dependencyOverlays =
-        /*
-    (import ./overlays inputs) ++
-    */
-        [
-            # see :help nixCats.flake.outputs.overlays
-            # This overlay grabs all the inputs named in the format
-            # `plugins-<pluginName>`
-            # Once we add this overlay to our nixpkgs, we are able to
-            # use `pkgs.neovimPlugins`, which is a set of our plugins.
-            (utils.standardPluginOverlay inputs)
-            # add any flake overlays here.
-
-            # when other people mess up their overlays by wrapping them with system,
-            # you may instead call this function on their overlay.
-            # it will check if it has the system in the set, and if so return the desired overlay
-            # (utils.fixSystemizedOverlay inputs.codeium.overlays
-            #   (system: inputs.codeium.overlays.${system}.default)
-            # )
-        ];
-
-    categoryDefinitions = {
-        pkgs,
-        settings,
-        categories,
-        extra,
-        name,
-        mkPlugin,
-        ...
-    } @ packageDef: {
+    extra_pkg_config.allowUnfree = true;
+    dependencyOverlays = [(utils.standardPluginOverlay inputs)];
+    categoryDefinitions = {pkgs, ...}: {
         lspsAndRuntimeDeps = with pkgs; {
             general = [
-                # emmylua-ls
+                emmylua-ls
                 marksman
                 nixd
                 taplo
@@ -52,7 +22,6 @@
                 sqruff
             ];
         };
-
         startupPlugins = with pkgs.vimPlugins; let
             fff-with-lib = pkgs.vimUtils.buildVimPlugin {
                 pname = "fff-nvim";
@@ -67,14 +36,11 @@
                 '';
             };
         in {
-            theme = let
-                themeMap = {
-                    kanso = [pkgs.neovimPlugins.kanso];
-                    techbase = [pkgs.neovimPlugins.techbase];
-                    llanura = ["llanura"];
-                };
-            in
-                themeMap.${themeConfig.theme};
+            theme = [
+                pkgs.neovimPlugins.kanso
+                pkgs.neovimPlugins.techbase
+                "llanura"
+            ];
             general = [
                 plenary-nvim
                 oil-nvim
@@ -121,19 +87,11 @@
         extraWrapperArgs = {};
         python3.libraries = {};
         extraLuaPackages = {};
-        environmentVariables = {
-            test = {
-                CATTESTVAR = "It worked!";
-            };
-        };
+        environmentVariables = {};
     };
 
     packageDefinitions = {
-        nvim = {
-            pkgs,
-            name,
-            ...
-        }: {
+        nvim = {pkgs, ...}: {
             settings = {
                 suffix-path = true;
                 suffix-LD = true;
@@ -149,16 +107,12 @@
             categories = {
                 general = true;
                 theme = true;
-                test = true;
             };
-            extra = {
-                colorscheme = themeConfig.nvimColorscheme;
-            };
+            extra.colorscheme = themeConfig.nvimColorscheme;
         };
     };
     defaultPackageName = "nvim";
 in
-    # see :help nixCats.flake.outputs.exports
     forEachSystem (system: let
         nixCatsBuilder = utils.baseBuilder luaPath {
             inherit system dependencyOverlays extra_pkg_config nixpkgs;
@@ -166,29 +120,19 @@ in
         categoryDefinitions
         packageDefinitions;
         defaultPackage = nixCatsBuilder defaultPackageName;
-        # this is just for using utils such as pkgs.mkShell
-        # The one used to build neovim is resolved inside the builder
-        # and is passed to our categoryDefinitions and packageDefinitions
         pkgs = import nixpkgs {inherit system;};
     in {
-        # this will make a package out of each of the packageDefinitions defined above
-        # and set the default package to the one passed in here.
         packages = utils.mkAllWithDefault defaultPackage;
-
-        # choose your package for devShell
-        # and add whatever else you want in it.
         devShells = {
             default = pkgs.mkShell {
                 name = defaultPackageName;
                 packages = [defaultPackage];
                 inputsFrom = [];
-                shellHook = ''
-                '';
+                shellHook = "";
             };
         };
     })
     // (let
-        # we also export a nixos module to allow reconfiguration from configuration.nix
         nixosModule = utils.mkNixosModules {
             moduleNamespace = [defaultPackageName];
             inherit
@@ -201,7 +145,6 @@ in
                 nixpkgs
                 ;
         };
-        # and the same for home manager
         homeModule = utils.mkHomeModules {
             moduleNamespace = [defaultPackageName];
             inherit
@@ -215,22 +158,14 @@ in
                 ;
         };
     in {
-        # these outputs will be NOT wrapped with ${system}
-
-        # this will make an overlay out of each of the packageDefinitions defined above
-        # and set the default overlay to the one named here.
         overlays = utils.makeOverlays luaPath {
-            # we pass in the things to make a pkgs variable to build nvim with later
             inherit nixpkgs dependencyOverlays extra_pkg_config;
-            # and also our categoryDefinitions
         }
         categoryDefinitions
         packageDefinitions
         defaultPackageName;
-
         nixosModules.default = nixosModule;
         homeModules.default = homeModule;
-
         inherit utils nixosModule homeModule;
         inherit (utils) templates;
     })
