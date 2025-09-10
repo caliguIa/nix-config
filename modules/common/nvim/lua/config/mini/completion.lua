ffee63e38245c63fce92ba64fbb1f5d73d80577a
Cal <acc@calrichards.io>
2025-09-10 13:38:26 +0100

    nvim completion fix in fff input

--- modules/common/nvim/lua/config/mini/completion.lua
+++ modules/common/nvim/lua/config/mini/completion.lua
@@ -2,19 +2,22 @@ require('mini.completion').setup({
     lsp_completion = { source_func = 'omnifunc', auto_setup = false },
 })
 
-Util.au.cmd('LspAttach', {
-    group = Util.au.group('lsp-attach-mini-comp'),
-    callback = function(args) vim.bo[args.buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp' end,
-})
-Util.au.cmd('WinEnter', {
-    group = Util.au.group('mini-comp-fff'),
-    pattern = { 'fff_input', 'fff_list', 'fff_file_info', 'fff_preview' },
-    callback = function() vim.b.minicompletion_disable = true end,
-})
 vim.lsp.config('*', {
     capabilities = vim.tbl_deep_extend(
         'force',
         vim.lsp.protocol.make_client_capabilities(),
         MiniCompletion.get_lsp_capabilities()
     ),
+})
+
+Util.au.cmd('LspAttach', {
+    group = Util.au.group('lsp-attach-mini-comp'),
+    callback = function(args) vim.bo[args.buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp' end,
+})
+
+Util.au.cmd('WinEnter', {
+    group = Util.au.group('mini-comp-fff'),
+    callback = function()
+        if vim.bo.filetype == 'fff_input' then vim.b.minicompletion_disable = true end
+    end,
 })
