307c33f3a787f376db733ef2c4a850caaaba8396
Cal <acc@calrichards.io>
2025-10-21 12:31:40 +0100

    use new extui nvim

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -3,4 +3,12 @@ vim.g.maplocalleader = vim.keycode('<space>')
 vim.loader.enable()
 vim.o.exrc = true
 vim.cmd.packadd('nvim.undotree')
+vim.schedule(function()
+    require('vim._extui').enable({
+        enable = true,
+        msg = {
+            target = 'msg', -- for now I'm happy with 'cmd'; 'box' seems buggy
+        },
+    })
+end)
 _G.nixCats = require('nixCats')
