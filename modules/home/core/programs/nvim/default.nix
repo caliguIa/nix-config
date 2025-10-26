8aacc0f19c8e4effbe864b58a1ea630f3e8e6e88
Cal <acc@calrichards.io>
2025-10-26 19:28:08 +0000

    fix: massively refactor nvim setup

--- modules/home/core/programs/nvim/default.nix
+++ modules/home/core/programs/nvim/default.nix
@@ -1,62 +1,26 @@
 {inputs, ...}: {
     flake.modules.homeManager.core = {
         pkgs,
-        lib,
+        config,
         ...
     }: {
-        config = let
-            packageName = "nvim";
-            plugins = with pkgs.vimPlugins; [
-                kanso-nvim
-                plenary-nvim
-                oil-nvim
-                mini-nvim
-                nvim-lspconfig
-                conform-nvim
-                git-conflict-nvim
-                vim-test
-                vim-tmux-navigator
-                nvim-bqf
-                neogit
-                fff-nvim
-                codecompanion-nvim
-                nvim-treesitter
-                nvim-treesitter.withAllGrammars
-                nvim-treesitter-textobjects
-                ts-comments-nvim
-                nvim-ts-autotag
-                aerial-nvim
-            ];
-            customPlugins = with pkgs; [
-                (vimUtils.buildVimPlugin {
-                    name = "indentmini.nvim";
-                    src = fetchFromGitHub {
-                        owner = "nvimdev";
-                        repo = "indentmini.nvim";
-                        rev = "main";
-                        hash = "sha256-YDO48kLOkzbJ9HDiQ0rQ/bbvyD8FQ9iNfSl1V0naUAs=";
-                    };
-                })
-                (vimUtils.buildVimPlugin {
-                    name = "zendiagram.nvim";
-                    src = fetchFromGitHub {
-                        owner = "caliguIa";
-                        repo = "zendiagram.nvim";
-                        rev = "main";
-                        hash = "sha256-vJMoeMUAKliPy9MF6oUd+gSQ1cNXPCEcju8OXEMvYew=";
-                    };
-                })
-                # (vimUtils.buildVimPlugin {
-                #     name = "timber.nvim";
-                #     src = fetchFromGitHub {
-                #         owner = "Goose97";
-                #         repo = "timber.nvim";
-                #         rev = "main";
-                #         hash = "";
-                #     };
-                # })
-            ];
-            runtimeDeps = with pkgs; [
+        xdg.configFile."nvim".source =
+            config.lib.file.mkOutOfStoreSymlink
+            "${config.home.homeDirectory}/nix-config/modules/home/core/programs/nvim/lua";
+        programs.neovim = {
+            enable = true;
+            package = inputs.nvim-nightly.packages.${pkgs.system}.neovim;
+            viAlias = true;
+            vimAlias = true;
+            vimdiffAlias = true;
+            withNodeJs = false;
+            withPython3 = false;
+            withRuby = false;
+            # extraWrapperArgs = [
+            #     "--set"
+            #     "NVIM_PACKPATH"
+            # ];
+            extraPackages = with pkgs; [
                 emmylua-ls
                 marksman
                 nixd
@@ -70,50 +34,57 @@
                 stylua
                 sqruff
             ];
-            foldPlugins = builtins.foldl' (
-                acc: next:
-                    acc
-                    ++ [next]
-                    ++ (foldPlugins (next.dependencies or []))
-            ) [];
-            pluginsWithDeps = lib.unique (foldPlugins plugins);
-            allPlugins = pluginsWithDeps ++ customPlugins;
-            configPlugin = pkgs.runCommandLocal "nvim-config" {} ''
-                mkdir -p $out
-                cp -r ${./.}/* $out/
-            '';
-            packpath = pkgs.runCommandLocal "packpath" {} ''
-                mkdir -p $out/pack/${packageName}/start
-                ln -vsfT ${configPlugin} $out/pack/${packageName}/start/nvim-config
-                ${lib.concatMapStringsSep "\n"
-                (plugin: "ln -vsfT ${plugin} $out/pack/${packageName}/start/${lib.getName plugin}")
-                allPlugins}
-            '';
-            nvim = pkgs.symlinkJoin {
-                name = "nvim";
-                paths = [inputs.nvim-nightly.packages.${pkgs.system}.neovim];
-                nativeBuildInputs = [pkgs.makeWrapper];
-                postBuild = ''
-                    wrapProgram $out/bin/nvim \
-                      --add-flags '-u' \
-                      --add-flags 'NORC' \
-                      --add-flags '--cmd' \
-                      --add-flags "'set packpath^=${packpath} | set runtimepath^=${packpath}'" \
-                      --set-default NVIM_APPNAME nvim \
-                      --set NVIM_PACKPATH ${packpath} \
-                      --prefix PATH : ${lib.makeBinPath runtimeDeps}
-                    ln -sf $out/bin/nvim $out/bin/vi
-                    ln -sf $out/bin/nvim $out/bin/vim
-                    ln -sf $out/bin/nvim $out/bin/bim
-                '';
-                passthru = {
-                    inherit packpath configPlugin;
-                    plugins.start = allPlugins;
-                    tools = runtimeDeps;
-                };
-            };
-        in {
-            home.packages = [nvim];
+            plugins = with pkgs.vimPlugins;
+                [
+                    kanso-nvim
+                    plenary-nvim
+                    oil-nvim
+                    mini-nvim
+                    nvim-lspconfig
+                    conform-nvim
+                    git-conflict-nvim
+                    vim-test
+                    vim-tmux-navigator
+                    nvim-bqf
+                    neogit
+                    fff-nvim
+                    codecompanion-nvim
+                    nvim-treesitter
+                    nvim-treesitter.withAllGrammars
+                    nvim-treesitter-textobjects
+                    ts-comments-nvim
+                    nvim-ts-autotag
+                    aerial-nvim
+                ]
+                ++ [
+                    (pkgs.vimUtils.buildVimPlugin {
+                        name = "indentmini.nvim";
+                        src = pkgs.fetchFromGitHub {
+                            owner = "nvimdev";
+                            repo = "indentmini.nvim";
+                            rev = "main";
+                            hash = "sha256-YDO48kLOkzbJ9HDiQ0rQ/bbvyD8FQ9iNfSl1V0naUAs=";
+                        };
+                    })
+                    (pkgs.vimUtils.buildVimPlugin {
+                        name = "zendiagram.nvim";
+                        src = pkgs.fetchFromGitHub {
+                            owner = "caliguIa";
+                            repo = "zendiagram.nvim";
+                            rev = "main";
+                            hash = "sha256-vJMoeMUAKliPy9MF6oUd+gSQ1cNXPCEcju8OXEMvYew=";
+                        };
+                    })
+                    # (pkgs.vimUtils.buildVimPlugin {
+                    #     name = "timber.nvim";
+                    #     src = pkgs.fetchFromGitHub {
+                    #         owner = "Goose97";
+                    #         repo = "timber.nvim";
+                    #         rev = "main";
+                    #         hash = "";
+                    #     };
+                    # })
+                ];
         };
     };
 }
