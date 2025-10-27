373303a7bfaad70f7b5be3898f5f5594397cecaa
Cal <acc@calrichards.io>
2025-10-27 11:12:53 +0000

    set xdg envs

--- flake.nix
+++ flake.nix
@@ -5,31 +5,19 @@
         nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
         flake-parts.url = "github:hercules-ci/flake-parts";
         import-tree.url = "github:vic/import-tree";
-        nix-darwin = {
-            url = "github:LnL7/nix-darwin";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+        nix-darwin.url = "github:LnL7/nix-darwin";
+        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+        home-manager.url = "github:nix-community/home-manager";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
         nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-        homebrew-cask = {
-            url = "github:homebrew/homebrew-cask";
-            flake = false;
-        };
-        fonts = {
-            url = "git+ssh://git@github.com/caliguIa/fonts";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        stylix = {
-            url = "github:nix-community/stylix";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        nvim-nightly = {
-            url = "github:nix-community/neovim-nightly-overlay";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+        homebrew-cask.url = "github:homebrew/homebrew-cask";
+        homebrew-cask.flake = false;
+        fonts.url = "git+ssh://git@github.com/caliguIa/fonts";
+        fonts.inputs.nixpkgs.follows = "nixpkgs";
+        stylix.url = "github:nix-community/stylix";
+        stylix.inputs.nixpkgs.follows = "nixpkgs";
+        nvim-nightly.url = "github:nix-community/neovim-nightly-overlay";
+        nvim-nightly.inputs.nixpkgs.follows = "nixpkgs";
         nix-minecraft.url = "github:Infinidoge/nix-minecraft";
     };
 }
--- modules/nixos/core/shell.nix
+++ modules/nixos/core/shell.nix
@@ -1,9 +1,5 @@
-{
-    self,
-    config,
-    ...
-}: let
-    users = config.flake.meta.users;
+topLevel @ {self, ...}: let
+    users = topLevel.config.flake.meta.users;
 in {
     flake.modules.darwin.core = {config, ...}: let
         homeDirectory = config.users.users.${users.primary}.home;
@@ -27,13 +23,22 @@ in {
         imports = [self.modules.generic.system-core-shell];
     };
 
-    flake.modules.generic.system-core-shell = {pkgs, ...}: {
+    flake.modules.generic.system-core-shell = {
+        config,
+        pkgs,
+        ...
+    }: let
+        homeDirectory = config.users.users.${users.primary}.home;
+    in {
         programs.fish.enable = true;
         users.users.${users.primary}.shell = pkgs.fish;
         environment = {
             shells = [pkgs.fish];
             variables = {
                 EDITOR = "nvim";
+                XDG_CACHE_HOME = "${homeDirectory}/.cache";
+                XDG_CONFIG_HOME = "${homeDirectory}/.config";
+                XDG_DATA_HOME = "${homeDirectory}/.local/share";
             };
         };
     };
