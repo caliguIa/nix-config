c22cfde2b9c7d6c2a45e07af98f0652e6ca6f19c
Cal <acc@calrichards.io>
2026-02-17 19:03:33 +0000

    slack

--- modules/hosts/karla/packages.nix
+++ modules/hosts/karla/packages.nix
@@ -2,7 +2,12 @@
     flake.modules.nixos.host_karla = {pkgs, ...}: {
         environment.systemPackages = with pkgs; [
             spotify
-            slack
+            (slack.overrideAttrs (old: {
+                postInstall = (old.postInstall or "") + ''
+                    substituteInPlace $out/share/applications/slack.desktop \
+                        --replace-fail "-s %U" "--ozone-platform=wayland --ozone-platform-hint=wayland -s %U"
+                '';
+            }))
             google-chrome
             unzip
         ];
