c640c516bb78649f898b503adbd4e2f8f414dbdf
Cal <acc@calrichards.io>
2026-07-22 14:00:01 +0100

    gitu config

--- modules/home/core/git.nix
+++ modules/home/core/git.nix
@@ -9,6 +9,7 @@
 
         home = config.directory;
         yaml = pkgs.formats.yaml {};
+        toml = pkgs.formats.toml {};
 
         # gh's extension dir expects each extension as a directory containing
         # its executable; symlink each package's bin/ into place.
@@ -84,6 +85,131 @@
                                 command = "gh enhance -R {{.RepoName}} {{.PrNumber}}\n";
                             }
                         ];
+                    };
+                };
+            };
+
+            "gitu/config.toml" = {
+                generator = toml.generate "gitu-config.toml";
+                value = {
+                    style = {
+                        separator.mods = "DIM";
+                        info_msg = {
+                            fg = "green";
+                            mods = "BOLD";
+                        };
+                        error_msg = {
+                            fg = "red";
+                            mods = "BOLD";
+                        };
+                        command = {
+                            fg = "blue";
+                            mods = "BOLD";
+                        };
+                        menu = {
+                            heading = {
+                                fg = "yellow";
+                                mods = "BOLD";
+                            };
+                            key = {
+                                fg = "magenta";
+                                mods = "BOLD";
+                            };
+                            active_arg = {
+                                fg = "light red";
+                                mods = "BOLD";
+                            };
+                            inactive_arg = {};
+                        };
+                        prompt = {
+                            fg = "cyan";
+                            mods = "BOLD";
+                        };
+                        section_header = {
+                            fg = "yellow";
+                            mods = "BOLD";
+                        };
+                        file_header = {
+                            fg = "magenta";
+                            mods = "BOLD";
+                        };
+                        hunk_header = {
+                            fg = "blue";
+                            bg = "#2a2e36";
+                        };
+                        diff_highlight = {
+                            tag_old = {
+                                fg = "red";
+                                mods = "";
+                            };
+                            tag_new = {
+                                fg = "green";
+                                mods = "";
+                            };
+                            changed_old = {
+                                bg = "#43242b";
+                                mods = "BOLD";
+                            };
+                            changed_new = {
+                                bg = "#2b3328";
+                                mods = "BOLD";
+                            };
+                        };
+                        syntax_highlight = {
+                            enabled = true;
+                            attribute.fg = "yellow";
+                            comment.fg = "gray";
+                            constant_builtin = {};
+                            constant = {};
+                            constructor = {};
+                            embedded = {};
+                            function_builtin.fg = "cyan";
+                            function.fg = "blue";
+                            keyword.fg = "magenta";
+                            number = {};
+                            module.fg = "cyan";
+                            property = {};
+                            operator = {};
+                            punctuation_bracket = {};
+                            punctuation_delimiter = {};
+                            string_special.fg = "yellow";
+                            string.fg = "yellow";
+                            tag = {};
+                            type.fg = "yellow";
+                            type_builtin.fg = "yellow";
+                            variable_builtin = {};
+                            variable_parameter = {};
+                        };
+                        picker = {
+                            prompt.fg = "cyan";
+                            info.mods = "DIM";
+                            selection_line.mods = "BOLD";
+                            matched = {
+                                fg = "yellow";
+                                mods = "BOLD";
+                            };
+                        };
+                        cursor = {
+                            symbol = "▌";
+                            fg = "yellow";
+                        };
+                        selection_bar = {
+                            symbol = "▌";
+                            fg = "yellow";
+                        };
+                        selection_line.bg = "#2a2e36";
+                        selection_area.bg = "#2a2e36";
+                        hash.fg = "yellow";
+                        branch = {
+                            fg = "green";
+                            mods = "BOLD";
+                        };
+                        remote.fg = "red";
+                        tag.fg = "yellow";
+                        blame = {
+                            line_num.mods = "DIM";
+                            code_line.mods = "DIM";
+                        };
                     };
                 };
             };
