38bc90ffef3fcd4fe20fe7486ab18c8988d7cc50
Cal <acc@calrichards.io>
2026-07-24 12:34:59 +0100

    nvim nixd lsp config

--- modules/home/core/nvim/lua/after/lsp/nixd.lua
+++ modules/home/core/nvim/lua/after/lsp/nixd.lua
@@ -1,17 +1,19 @@
-local flake = '(builtins.getFlake ("git+file://" + toString ./.))'
+local flake_dir = vim.uv.os_homedir() .. '/nix-config'
+local host = vim.uv.os_gethostname()
+
+local flake = '(builtins.getFlake "' .. flake_dir .. '")'
+local host_options = flake .. '.nixosConfigurations.' .. host .. '.options'
 
 ---@type vim.lsp.Config
 return {
     settings = {
         nixd = {
-            extraOptions = '--impure',
             nixpkgs = { expr = 'import ' .. flake .. '.inputs.nixpkgs { }' },
             options = {
-                nixos = { expr = flake .. '.nixosConfigurations.karla.options' },
-                ['home-manager'] = {
-                    expr = flake .. '.nixosConfigurations.karla.options.home-manager.users.type.getSubOptions []',
+                nixos = { expr = host_options },
+                hjem = {
+                    expr = host_options .. '.hjem.users.type.getSubOptions []',
                 },
-                flake = { expr = flake .. '.outputs' },
             },
         },
     },
