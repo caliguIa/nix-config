31f1fe353f858698cda58237e8187618b624eae8
Cal <acc@calrichards.io>
2026-07-29 10:38:49 +0100

    flake.lock: Update

--- modules/hosts/karla/network.nix
+++ modules/hosts/karla/network.nix
@@ -1,51 +1,52 @@
 {
     flake.modules.nixos.host_karla = let
         hostname = "karla";
-    in {
-        networking = {
-            hostName = hostname;
-            firewall = {
-                allowedTCPPortRanges = [
-                    {
-                        from = 1714;
-                        to = 1764;
-                    }
-                ];
-                allowedUDPPortRanges = [
-                    {
-                        from = 1714;
-                        to = 1764;
-                    }
-                ];
+    in
+        {lib, ...}: {
+            networking = {
+                hostName = hostname;
+                firewall = {
+                    allowedTCPPortRanges = [
+                        {
+                            from = 1714;
+                            to = 1764;
+                        }
+                    ];
+                    allowedUDPPortRanges = [
+                        {
+                            from = 1714;
+                            to = 1764;
+                        }
+                    ];
+                };
+                networkmanager = {
+                    enable = true;
+                    dns = lib.mkForce "none";
+                };
+                nameservers = ["127.0.0.1" "1.1.1.1"];
             };
-            networkmanager = {
+            services.avahi = {
                 enable = true;
-                dns = "none";
+                nssmdns4 = true;
             };
-            nameservers = ["127.0.0.1" "1.1.1.1"];
-        };
-        services.avahi = {
-            enable = true;
-            nssmdns4 = true;
-        };
-        services.dnsmasq = {
-            enable = true;
-            settings = {
-                address = ["/local.oneupsales.dev/127.0.0.3"];
-                server = [
-                    "1.1.1.1"
-                    "8.8.8.8"
-                ];
-                no-resolv = true;
+            services.dnsmasq = {
+                enable = true;
+                settings = {
+                    address = ["/local.oneupsales.dev/127.0.0.3"];
+                    server = [
+                        "1.1.1.1"
+                        "8.8.8.8"
+                    ];
+                    no-resolv = true;
+                };
+            };
+            # Tailscale for reaching smiley (SSH/SMB/admin UIs) over the tailnet.
+            # Bring up with `sudo tailscale up --accept-dns=false` so tailscaled
+            # leaves karla's dnsmasq/resolv.conf setup alone (we use public DNS
+            # for *.smiley.calrichards.io, not MagicDNS).
+            services.tailscale = {
+                enable = true;
+                openFirewall = true;
             };
         };
-        # Tailscale for reaching smiley (SSH/SMB/admin UIs) over the tailnet.
-        # Bring up with `sudo tailscale up --accept-dns=false` so tailscaled
-        # leaves karla's dnsmasq/resolv.conf setup alone (we use public DNS
-        # for *.smiley.calrichards.io, not MagicDNS).
-        services.tailscale = {
-            enable = true;
-            openFirewall = true;
-        };
-    };
 }
