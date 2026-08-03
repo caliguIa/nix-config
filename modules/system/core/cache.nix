c33e203c630dec3efcc6f83d4c60391c30fa8d0f
Cal <acc@calrichards.io>
2026-08-03 09:45:00 +0100

    flake.lock: Update

--- modules/system/core/cache.nix
+++ modules/system/core/cache.nix
@@ -7,14 +7,8 @@
                 "@wheel"
             ];
             substituters = ["https://cache.nixos.org/"];
-            extra-substituters = [
-                "https://cache.garnix.io"
-                "https://nix-community.cachix.org"
-            ];
-            extra-trusted-public-keys = [
-                "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            ];
+            extra-substituters = ["https://nix-community.cachix.org"];
+            extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
             fallback = true;
             connect-timeout = 15;
             stalled-download-timeout = 10;
